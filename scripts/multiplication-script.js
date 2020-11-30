89ba2cb526a11b627bb20725d1f45708870151b1
Geomty <69478549+Geomty@users.noreply.github.com>
2020-11-29 20:56:27 -0500

    Update multiplication-script.js

--- scripts/multiplication-script.js
+++ scripts/multiplication-script.js
@@ -1,9 +1,7 @@
 function multiplicationFunction() {
     var numberOne = prompt("What is your first number?");
     var numberTwo = prompt("What is your second number?");
-    if (isNaN(numberOne) || isNaN(numberTwo)) {
-        return alert('You must enter a number.');
-    } else if (!numberOne.length || !numberTwo.length) {
+    if (typeof numberOne == 'object' || typeof numberTwo == 'object' || !numberOne.length || !numberTwo.length || isNaN(numberOne) || isNaN(numberTwo)) {
         return alert('You must enter a number.');
     }
     alert(`Your answer: ${parseInt(numberOne) * parseInt(numberTwo)}`);
