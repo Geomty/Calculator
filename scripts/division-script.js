91215198005ca6c03ba6c1ed3d18066aef0f7fbf
Geomty <69478549+Geomty@users.noreply.github.com>
2020-11-29 20:55:58 -0500

    Update division-script.js

--- scripts/division-script.js
+++ scripts/division-script.js
@@ -1,9 +1,7 @@
 function divisionFunction() {
     var numberOne = prompt("What is your first number?");
     var numberTwo = prompt("What is your second number?");
-    if (isNaN(numberOne) || isNaN(numberTwo)) {
-        return alert('You must enter a number.');
-    } else if (!numberOne.length || !numberTwo.length) {
+    if (typeof numberOne == 'object' || typeof numberTwo == 'object' || !numberOne.length || !numberTwo.length || isNaN(numberOne) || isNaN(numberTwo)) {
         return alert('You must enter a number.');
     } else if (numberTwo == '0') {
         return alert('You cannot divide by zero.');
